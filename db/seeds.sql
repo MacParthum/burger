45c763d5a49a9b796d537cf343bf409fe7f51906
Mac Parthum <macparthum@gmail.com>
2020-09-10 20:04:32 -0400

    fixed typos, added server.js file(not sure how it dissapeard. fixed post route

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Jalepeno Cheese Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Soy Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Node.js Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mysql Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Cyanide Burger');
